e25d42774f694562f99cffc630b6ddbec93ac4c5
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-09-26 16:55:13 +0900

    bakery데이터 가져오기 수정

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,18 +1,30 @@
 import Review from "@/app/component/review.js";
 
-export default async function Read(props){
-    let param = props.params.id
-    const resp = await fetch(`${process.env.API_URL}/api/bakeries/${props.params.id}?populate=*`);
+const fetchBakeries = async (params) => {
+    const resp = await fetch(`${process.env.API_URL}/api/bakeries/${params.id}?populate=*`);
     const bakery = await resp.json();
-    const resp2 = await fetch(`${process.env.API_URL}/api/reviews?filters[bakery][id][$eq]=${props.params.id}`);
-    const review = await resp2.json();
+
+    return bakery
+}
+
+const fetchReviews = async (params) => {
+    const resp = await fetch(`${process.env.API_URL}/api/reviews?filters[bakery][id][$eq]=${params.id}`);
+    const review = await resp.json();
+
+    return review
+}
+
+const Read = async({ params }) => {
+    const bakery = await fetchBakeries(params);
+    const review = await fetchReviews(params);
 
     return(
         <>
         <h2>{bakery.data.attributes.name}</h2>
         <p>{bakery.data.attributes.description}</p>
-        <Review param={param} review={review}/>
-        
+        <Review param={bakery.data.id} review={review}/>
         </>
     )
 }
+
+export default Read;
