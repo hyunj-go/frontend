1e6ed33c732f45ab31cd93cf8cfbe4c2c03bef8b
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-09-27 09:19:18 +0900

    API_URL

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,14 +1,14 @@
 import Review from "@/app/component/review.js";
 
 const fetchBakeries = async (params) => {
-    const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/bakeries/${params.id}?populate=*`);
+    const resp = await fetch(`${process.env.API_URL}/api/bakeries/${params.id}?populate=*`);
     const bakery = await resp.json();
 
     return bakery
 }
 
 const fetchReviews = async (params) => {
-    const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/reviews?filters[bakery][id][$eq]=${params.id}`);
+    const resp = await fetch(`${process.env.API_URL}/api/reviews?filters[bakery][id][$eq]=${params.id}`);
     const review = await resp.json();
 
     return review
