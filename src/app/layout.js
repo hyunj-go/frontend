8242ac9177889ab909c8ebb548e3c00e23767ac2
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-11-01 12:46:33 +0900

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
+"use client"
 import './globals.css';
-// import 'public/css/style.css';
+import 'public/css/style.css';
 import SearchInput from './component/SearchInput';
 
 export const metadata = {
