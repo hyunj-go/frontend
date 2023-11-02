11907c63502363316963d1f24e58f061def27f48
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-11-02 13:32:46 +0900

    Update SearchInput.js

--- src/app/component/SearchInput.js
+++ src/app/component/SearchInput.js
@@ -1,16 +1,17 @@
 "use client"
 import { BsSearch } from "react-icons/bs";
 import { TiDelete } from "react-icons/ti";
-// import {useCallback, useEffect, useState } from "react"
+import {useCallback, useEffect, useState } from "react"
 import { useRouter, useSearchParams, usePathname } from "next/navigation";
 
 //input에 검색어를 적을때마다 page이동하면서 검색어와 page값이 query로 전해진다
-const SearchInput = ({ name }) => {
+const SearchInput = () => {
     const router = useRouter();
     const pathName = usePathname();
     const searchParams = useSearchParams();
     // const [search, setSearch] = useState('')
     // let [currentPathName, setCurrentPathName] = useState('/');
+    let [search, setSearch] = useState('');
 
     const handleSearchValue = (e) => { 
         // now you got a read/write object
@@ -25,12 +26,12 @@ const SearchInput = ({ name }) => {
         }
 
         // cast to string
-        const search = current.toString();
+        setSearch(current.toString());
         const query = search ? `?${search}` : "";
 
         
         // router.push(`${pathname}${query}`);
-        router.replace(`/search${query}`, {shallow: true});
+        router.replace(`/search${query}`);
 
     }
     //검색어 지우기
@@ -38,13 +39,16 @@ const SearchInput = ({ name }) => {
         // setSearch('')
     }
 
+
+
+    
     
 
     return (
         <>
             <div>
-                <input type='text' placeholder='검색어를 입력하세요' autoFocus autoComplete='off' value={name} onChange={handleSearchValue} />
-                {name && <TiDelete onClick={cleanSearch} size={20}/>}
+                <input type='text' placeholder='검색어를 입력하세요' autoFocus autoComplete='off' value={search} onChange={handleSearchValue} />
+                {search && <TiDelete onClick={cleanSearch} size={20}/>}
                 <BsSearch size={20} />
             </div>
         </>
