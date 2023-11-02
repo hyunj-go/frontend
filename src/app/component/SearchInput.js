c5b8928474fe7d6aa664cf60cc725b9f7bba5fce
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-11-02 13:36:48 +0900

    Update SearchInput.js

--- src/app/component/SearchInput.js
+++ src/app/component/SearchInput.js
@@ -11,13 +11,13 @@ const SearchInput = () => {
     const searchParams = useSearchParams();
     // const [search, setSearch] = useState('')
     // let [currentPathName, setCurrentPathName] = useState('/');
-    let [search, setSearch] = useState('');
+    let [value, setValue] = useState('');
 
     const handleSearchValue = (e) => { 
         // now you got a read/write object
         const current = new URLSearchParams(Array.from(searchParams.entries())); 
         // update as necessary
-        const value = e.target.value.trim();
+        setValue(e.target.value.trim());
 
         if (!value) {
             current.delete("name");
@@ -26,7 +26,7 @@ const SearchInput = () => {
         }
 
         // cast to string
-        setSearch(current.toString());
+        const search = current.toString();
         const query = search ? `?${search}` : "";
 
         
@@ -47,7 +47,7 @@ const SearchInput = () => {
     return (
         <>
             <div>
-                <input type='text' placeholder='검색어를 입력하세요' autoFocus autoComplete='off' value={search} onChange={handleSearchValue} />
+                <input type='text' placeholder='검색어를 입력하세요' autoFocus autoComplete='off' value={value} onChange={handleSearchValue} />
                 {search && <TiDelete onClick={cleanSearch} size={20}/>}
                 <BsSearch size={20} />
             </div>
