2535332e363436f3244688e31eb4b4e25a535483
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-11-09 16:52:59 +0900

    Update SearchInput.js

--- src/app/component/SearchInput.js
+++ src/app/component/SearchInput.js
@@ -13,18 +13,18 @@ const SearchInput = () => {
     let query = searchParams.get('name');
     let chk = false;
 
-
     const handleSearch = useCallback((e) => { 
         setSearchQuery(e.target.value);
+        handleRouter();
+    },[searchParams])
 
+    const handleRouter = useCallback((e) => {
         if(pathName !== '/search' && !chk){ console.log('search가 아님');
             chk = true;
             router.replace(`/search?name=${searchQuery}`);
         }else {
             router.push(`?name=${searchQuery}`);     
         }
-        
-
     }, [searchQuery])
 
     
