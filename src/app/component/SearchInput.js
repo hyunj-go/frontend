1f114153a8a1958755569bc2b905f4f291629931
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-11-10 09:32:32 +0900

    Update SearchInput.js

--- src/app/component/SearchInput.js
+++ src/app/component/SearchInput.js
@@ -13,19 +13,19 @@ const SearchInput = () => {
     //검색값이 변할때마다 새롭게 요청
     useEffect(() => {
         try {
-            const url = `?name=${search}`
-            router.replace(url)
-            setSearch(search);
+            // const url = `?name=${search}`
+            // router.replace(url)
+            // setSearch(search);
+            console.log('useEffect');
         }
         catch (e) {
             console.error(e.response)
         }
     }, [search])
 
-    //search 값이 바뀔때 재호출 (useCallback 쓰면 불러올때마다 함수 생성하지 않고 기존 함수 사용)
+    //search 값이 바뀔때 재호출 (useCallback 쓰면 랜딩할때마다 함수 생성하지 않고 기존 함수 사용)
     const handleSearchValue = useCallback((e) => { 
         setSearch(e.target.value)
-
     }, [search])
 
     //검색어 지우기
