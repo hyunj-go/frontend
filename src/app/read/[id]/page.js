585fc21a797debcfe30b58e5dcd49f8df1bfc54c
hyunj-p <103031060+hyunj-p@users.noreply.github.com>
2023-12-12 13:13:32 +0900

    Update page.js

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,44 +1,55 @@
-import Review from "@/app/component/review.js";
+// import Review from "@/app/component/review.js";
 
-const fetchBakeries = async (params) => {
-    try {
-        const resp = await fetch(`${process.env.API_URL}/api/bakeries/${params}?populate=*`, {
-            cache: "no-store",
-        });
-        const bakery = await resp.json();
-        return bakery
-    }catch(error){
-        console.log(error.stack);
-        return {}
-    }
-}
+// const fetchBakeries = async (params) => {
+//     try {
+//         const resp = await fetch(`${process.env.API_URL}/api/bakeries/${params}?populate=*`, {
+//             cache: "no-store",
+//         });
+//         const bakery = await resp.json();
+//         return bakery
+//     }catch(error){
+//         console.log(error.stack);
+//         return {}
+//     }
+// }
 
-const fetchReviews = async (params) => {
-    try {
-        const resp = await fetch(`${process.env.API_URL}/api/reviews?filters[bakery][id][$eq]=${params}`, {
-            cache: "no-store",
-        });
-        const review = await resp.json();
-        return review
-    }catch(error){
-        console.log(error.stack);
-        return {}
-    }
-}
+// const fetchReviews = async (params) => {
+//     try {
+//         const resp = await fetch(`${process.env.API_URL}/api/reviews?filters[bakery][id][$eq]=${params}`, {
+//             cache: "no-store",
+//         });
+//         const review = await resp.json();
+//         return review
+//     }catch(error){
+//         console.log(error.stack);
+//         return {}
+//     }
+// }
+
+// const BakeryIdPage = async(props) => {
+//     const bakery = await fetchBakeries(props.params.id);
+//     const review = await fetchReviews(props.params.id);
+
+//     return(
+//         <>  
+//             <h2>{bakery.data.attributes.name}</h2>
+//             <p>{bakery.data.attributes.description}</p>
+//             {bakery.data.attributes.image.data.map((bakeryImg)=>{
+//             return <li key={bakeryImg.id}><img src={bakeryImg.attributes.url} alt={bakery.data.attributes.name}/></li>
+//             })}
+//             <Review param={bakery.data.id} review={review}/>
+//         </>
+//     )
+// }
+
+// export default BakeryIdPage;
+
 
 const BakeryIdPage = async(props) => {
-    const bakery = await fetchBakeries(props.params.id);
-    const review = await fetchReviews(props.params.id);
-
     return(
-        <>  
-            <h2>{bakery.data.attributes.name}</h2>
-            <p>{bakery.data.attributes.description}</p>
-            {bakery.data.attributes.image.data.map((bakeryImg)=>{
-            return <li key={bakeryImg.id}><img src={bakeryImg.attributes.url} alt={bakery.data.attributes.name}/></li>
-            })}
-            <Review param={bakery.data.id} review={review}/>
-        </>
+                <> 
+                asdf
+                </>
     )
 }
 
