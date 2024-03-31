eeeb8e691f5c7dae2b9bca9b05a2a03348bd01e1
hyunj <hyu.nnprk@gmail.com>
2024-03-31 16:07:22 +0900

    style

--- src/app/layout.js
+++ src/app/layout.js
@@ -81,6 +81,49 @@ export default function RootLayout({ children }) {
             })}
           </ol> */}
           {children}
+
+          <div className="contact" id="contact">
+            <div className="container">
+              <div className="row">
+                <div className="three columns">
+                  <h6>Contact</h6>
+                  <ul>
+                    <li><a href="tel:7054707624">705–470–PNCH</a></li>
+                    <li><a href="mailto:">aaa@gmail.com</a></li>
+                  </ul>
+                </div>
+                <div className="three columns">
+                  <h6>Address</h6>
+                  <p className="footer-address">2037 Long Lake Rd, Block B, Unit 8B
+                    <br />Sudbury, ON P3E 6J9</p>
+                  <a href="https://goo.gl/maps/iFvNnzzMnow" target="_blank"><i className="fa fa-map-marker"
+                      aria-hidden="true"></i> Get Directions</a>
+                </div>
+                <div className="three columns">
+                  <h6>Hours of Operation</h6>
+                    <p>
+                      <strong>Tues–Fri:</strong> 8:30 a.m. – 4 p.m.<br />
+                      <strong>Sat:</strong> 9 a.m. – 3 p.m.<br />
+                      <strong>Sun-Mon:</strong> CLOSED<br />
+                    </p>
+                </div>
+                <div className="three columns">
+                  <h6>Follow</h6>
+                  <ul>
+                    <li><a href="http://www.facebook.com/pinchmans" target="_blank"><i className="fab fa-facebook-square" aria-hidden="true"></i> Facebook</a></li>
+                    <li><a href="http://www.instagram.com/pinchmans" target="_blank"><i className="fab fa-instagram" aria-hidden="true"></i> Instagram</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <footer>
+            <div className="container">
+              {/* <a data-scroll href="#"><img src="images/logo.svg" alt="logo" /></a> */}
+              <p>Copyright ©2024 Bakery in Seoul</p>
+            </div>
+          </footer>
         </div>
       </body>
     </html>
