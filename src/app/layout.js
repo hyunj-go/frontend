94a7787a6a115d759a0bc8366aa9bedf640ce41a
hyunj <hyu.nnprk@gmail.com>
2024-05-06 20:17:02 +0900

    수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import 'public/css/style.css';
 import 'public/css/content.css';
 import Link from 'next/link';
 import Nav from './component/Nav';
-import Nav from './component/Footer';
+import Footer from './component/Footer';
 import AuthSession from "./component/AuthSession";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBreadSlice } from "@fortawesome/free-solid-svg-icons";
